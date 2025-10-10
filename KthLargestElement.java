5a21f2f9116b05b9e36b3e90a2e684ba72b335d8
Syed Safi <syedisaf@gmail.com>
2025-10-10 23:37:46 +0530

    eleventh

---
+++ KthLargestElement.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class KthLargestElement {
+
+    public static int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>(k);
+
+        for (int num : nums) {
+            if (minHeap.size() < k) {
+                minHeap.add(num);
+            } else if (num > minHeap.peek()) {
+                minHeap.poll();
+                minHeap.add(num);
+            }
+        }
+
+        return minHeap.peek();
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {3, 2, 1, 5, 6, 4};
+        int k = 2;
+        int result = findKthLargest(nums, k);
+        System.out.println(result);
+    }
+}
